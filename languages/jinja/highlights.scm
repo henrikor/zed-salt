2c9856edf40a261b4c9cfe517797afe62434345c
Henrik Ormåsen <henrik.ormasen@gmail.com>
2026-07-30 18:20:18 +0200

    buggfix

--- languages/jinja/highlights.scm
+++ languages/jinja/highlights.scm
@@ -4,19 +4,48 @@
 (lit_string) @string
 (identifier) @variable
 
+(fn_call
+  fn_name: (identifier) @function)
+
+(kwarg
+  key: (identifier) @property)
+
+(pair
+  key: (lit_string) @property)
+
 (jinja_statement_open) @punctuation.special
 (jinja_statement_close) @punctuation.special
 (jinja_value_open) @punctuation.special
 (jinja_value_close) @punctuation.special
 (jinja_comment_open) @comment
-(jinja_comment_content) @comment
 (jinja_comment_close) @comment
+(jinja_comment_content) @comment
 
-["if" "elif" (jinja_else) "for" "in" "block" "endblock"
- "extends" "include" "import" "from" "as" "set" "macro"
- "endmacro" "call" "endcall" "filter" (jinja_raw) "endraw"] @keyword
+["(" ")" "[" "]" "{" "}"] @punctuation.bracket
+["," ":" "="] @punctuation.delimiter
 
-["and" "or" "not" "is"] @keyword.operator
+(comparison
+  operator: _ @operator)
 
-["+" "-" "*" "/" "//" "%" "**" "~"
- "==" "!=" ">" ">=" "<" "<=" "=" "|" "."] @operator
+["if" "elif" (jinja_else) "for" "in" "block" "endblock" "extends" "include" "import" "from" "as" "set" "macro" "endmacro" "call" "endcall" "filter" (jinja_raw) "endraw"] @keyword
+
+(jinja_block
+  block_name: (identifier) @function)
+
+(jinja_macro
+  macro_name: (identifier) @function)
+
+(jinja_filter
+  filter_name: (identifier) @function)
+
+(jinja_extends
+  parent_template: (lit_string) @string.special)
+
+(jinja_include
+  template: (lit_string) @string.special)
+
+(jinja_import
+  module: (lit_string) @string.special)
+
+(jinja_from
+  module: (lit_string) @string.special)
